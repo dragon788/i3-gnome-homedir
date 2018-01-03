8fd8b88e56cf51e9cf52b590086ea4b967685048
dragon788 <github@deik.me>
2018-01-03 03:46:56 +0000

    Need sleep to allow network to settle before next apt operation

--- homedir-install.sh
+++ homedir-install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -x
 echo "Copying configs to their respective locations and installing required packages"
 cp -r --parents .config .local .xsession $HOME
-sudo apt-get install --no-install-recommends gnome-flashback gdm3 network-manager-gnome -y
+sudo apt-get update
+sudo apt-get install gnome-flashback gdm3 network-manager-gnome -y
+sudo apt-get remove --purge lightdm
+# Line below "fixes" the fact that some Vagrant/preseeded machines may have an "unmanaged"
+# interface from before NetworkManager was installed
 sudo rm -rf /etc/network/interfaces.d/* && sudo service network-manager restart
+sleep 2
+# xserver-xorg brings in some other nice things like touchpad drivers
 sudo apt-get install i3 xserver-xorg -y
-sudo service gdm restart
+sudo apt-get autoremove
+#sudo service gdm restart
 echo "Install finished, reboot and select 'Default session' in gdm"
