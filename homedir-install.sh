b4273278b87737f2f091e8a5c270ce0f5ec29893
Ethan Spoelstra <espoelstra@pithoslabs.com>
2018-05-10 01:23:49 +0000

    Fixed up homedir-install.sh for minimalism

--- homedir-install.sh
+++ homedir-install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
+
+# This was for debugging which user the script gets executed as by 'vagrant provision' shell inline or path
+# echo $HOME
+# echo $PWD
+# ls -la
 echo "Copying configs to their respective locations and installing required packages"
 cp -r --parents .config .local .xsession $HOME
 sudo apt-get update
-sudo apt-get install gnome-flashback network-manager-gnome -y
+sudo apt-get install dbus-user-session gnome-flashback network-manager-gnome -y
 sudo apt-get install --no-install-recommends gdm3
 sudo apt-get remove --purge lightdm
 # Line below "fixes" the fact that some Vagrant/preseeded machines may have an "unmanaged"
@@ -13,4 +18,4 @@ sleep 10
 sudo apt-get install i3 rofi xserver-xorg -y
 sudo apt-get autoremove
 #sudo service gdm restart
-echo "Install finished, reboot and select 'Default session' in gdm"
+echo "Install finished, reboot and select 'System Default' in gdm"
